ccfbdc4f023af9af41a2fdb884f76d2b86269aad
Vladimir Egorov <90173654+JavaDrinker96@users.noreply.github.com>
2022-07-11 13:38:17 +0300

    - Add a new exception for missing data.

---
+++ src/main/java/com/example/moview/moview/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.moview.moview.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(final String message) {
+        super(message);
+    }
+}
