2b6ec4ab220bd24d6f99f3d66343ee4796a7cd6f
Vladimir Egorov <90173654+JavaDrinker96@users.noreply.github.com>
2022-07-28 12:58:26 +0300

    - Change annotations @RequestMapping  with simpler ones.

--- src/main/java/com/example/moview/moview/controller/MovieController.java
+++ src/main/java/com/example/moview/moview/controller/MovieController.java
@@ -9,16 +9,20 @@ import com.example.moview.moview.service.MovieService;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@RequestMapping("/movie")
 public class MovieController {
 
     private final MovieService movieService;
@@ -29,33 +33,33 @@ public class MovieController {
         this.modelMapper = modelMapper;
     }
 
-    @RequestMapping(value = "/movie", method = RequestMethod.POST)
+    @PostMapping
     public ResponseEntity<MovieShortDto> create(@RequestBody @Valid final MovieCreateDto dto) {
         final Movie movie = modelMapper.map(dto, Movie.class);
         final MovieShortDto movieShortDto = modelMapper.map(movieService.create(movie), MovieShortDto.class);
         return ResponseEntity.status(HttpStatus.OK).body(movieShortDto);
     }
 
-    @RequestMapping(value = "/movie/{id}", method = RequestMethod.GET)
+    @GetMapping("/{id}")
     public ResponseEntity<MovieDto> deepRead(@PathVariable final Long id) {
         final MovieDto movieDto = modelMapper.map(movieService.read(id), MovieDto.class);
         return ResponseEntity.status(HttpStatus.CREATED).body(movieDto);
     }
 
-    @RequestMapping(value = "/movie", method = RequestMethod.PUT)
+    @PutMapping
     public ResponseEntity<MovieDto> update(@RequestBody @Valid final MovieUpdateDto dto) {
         final Movie movie = modelMapper.map(dto, Movie.class);
         final MovieDto movieDto = modelMapper.map(movieService.update(movie), MovieDto.class);
         return ResponseEntity.status(HttpStatus.OK).body(movieDto);
     }
 
-    @RequestMapping(value = "/movie/{id}", method = RequestMethod.DELETE)
+    @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable final Long id) {
         movieService.delete(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-    @RequestMapping(value = "/movie/all", method = RequestMethod.GET)
+    @GetMapping("/all")
     public ResponseEntity<List<MovieShortDto>> readAll() {
         final List<MovieShortDto> movieShortDtoList = movieService.readAll().stream()
                 .map(x -> modelMapper.map(x, MovieShortDto.class)).toList();
--- src/main/java/com/example/moview/moview/controller/ReviewController.java
+++ src/main/java/com/example/moview/moview/controller/ReviewController.java
@@ -9,15 +9,18 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 @RestController
+@RequestMapping("/review")
 public class ReviewController {
 
     private final ReviewService reviewService;
@@ -29,21 +32,21 @@ public class ReviewController {
         this.modelMapper = modelMapper;
     }
 
-    @RequestMapping(value = "/review", method = RequestMethod.POST)
+   @PostMapping
     public ResponseEntity<ReviewDto> create(@RequestBody @Valid final ReviewCreateDto dto) {
         final Review review = modelMapper.map(dto, Review.class);
         final ReviewDto reviewDto = modelMapper.map(reviewService.create(review), ReviewDto.class);
         return ResponseEntity.status(HttpStatus.OK).body(reviewDto);
     }
 
-    @RequestMapping(value = "/review", method = RequestMethod.PUT)
+    @PutMapping
     public ResponseEntity<ReviewDto> update(@RequestBody @Valid final ReviewUpdateDto dto) {
         final Review review = modelMapper.map(dto, Review.class);
         final ReviewDto reviewDto = modelMapper.map(reviewService.update(review), ReviewDto.class);
         return ResponseEntity.status(HttpStatus.OK).body(reviewDto);
     }
 
-    @RequestMapping(value = "/review/{id}", method = RequestMethod.DELETE)
+    @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id) {
         reviewService.delete(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
